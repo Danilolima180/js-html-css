1bb89e1aa46aa4285998e7604ac32ff2cd5c15ce
Danilo <danilolima180@gmail.com>
2022-06-22 20:41:45 -0300

    declarado variaveis em JS

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+function showPassword() {
+const eye = document.getElementById('eye');
+const eyeslash = document.getElementById('eyeslash');
+const fieldPassword = document.getElementById('fiel-password');
+
+if () {
+
+}
+else {
+  
+}
+};
+  
